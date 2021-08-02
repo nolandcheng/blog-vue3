578b445c6b047e3163728fcae5551fed98d9f7ff
Mr.Cheng <511520980@qq.com>
2021-08-02 17:53:12 +0800

    test

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 已刷并标记为【简单】的题我就放在文件夹中不罗列出来了
 
 ## 算法
-[简单](arithmetic/easy)
+[简单系列](arithmetic/easy)
---
+++ arithmetic/easy/test.js
@@ -0,0 +1,7 @@
+/*
+ * @Descripttion:
+ * @Author: Cheng
+ * @Date: 2021-08-02 17:52:49
+ * @LastEditors: Cheng
+ * @LastEditTime: 2021-08-02 17:52:50
+ */
