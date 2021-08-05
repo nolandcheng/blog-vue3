e76387cf1516cf2f2cedc967e769513d06f09780
Mr.Cheng <511520980@qq.com>
2021-08-05 17:50:23 +0800

    init vue3 + vite2

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @Author: Cheng
+ * @Date: 2021-08-05 17:10:09
+ * @LastEditors: Cheng
+ * @LastEditTime: 2021-08-05 17:23:45
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template> 
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @Author: Cheng
+ * @Date: 2021-08-05 17:28:29
+ * @LastEditors: Cheng
+ * @LastEditTime: 2021-08-05 17:28:46
+ */
+// vite.config.js
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
